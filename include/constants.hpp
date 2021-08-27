5aa09016d2ac16d3452ab1f70a7ed24269071bed
Dylan Ascencio <empressglatia@gmail.com>
2021-08-27 14:43:38 -0500

    Added static_room_data JSON and modified constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -2,4 +2,5 @@
 
 #include <filesystem>
 
+extern std::filesystem::path RES_BASE_PATH;
 extern std::filesystem::path NAMES_BASE_PATH;
--- src/constants.cpp
+++ src/constants.cpp
@@ -1,3 +1,4 @@
 #include "constants.hpp"
 
-std::filesystem::path NAMES_BASE_PATH = std::filesystem::path("res") / "names";
+std::filesystem::path RES_BASE_PATH = std::filesystem::path("res");
+std::filesystem::path NAMES_BASE_PATH = RES_BASE_PATH / "names";
