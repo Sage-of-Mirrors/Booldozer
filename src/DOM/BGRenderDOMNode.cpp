8b30bbe460a4b262da2b557725fe5d4fcbc1d9f8
Leaf <spacecats64@gmail.com>
2021-09-06 16:39:44 -0400

    fix rotation

--- src/DOM/BGRenderDOMNode.cpp
+++ src/DOM/BGRenderDOMNode.cpp
@@ -14,7 +14,7 @@ void LBGRenderDOMNode::RenderBG(float dt)
 		//ATTENTION X and Z are SWAPPED!!!! Please fix when writing. TODO: Figure out why we need this.
 		m = glm::translate(m, glm::vec3(mPosition.z, mPosition.y, mPosition.x));
 		m = glm::rotate(m, glm::radians(mRotation.x), glm::vec3(1, 0, 0));
-		m = glm::rotate(m, glm::radians(mRotation.y), glm::vec3(0, 1, 0));
+		m = glm::rotate(m, glm::radians(-mRotation.y), glm::vec3(0, 1, 0));
 		m = glm::rotate(m, glm::radians(mRotation.z), glm::vec3(0, 0, 1));
 		m = glm::scale(m, mScale);
 		mTransform = std::make_shared<glm::mat4>(m);
