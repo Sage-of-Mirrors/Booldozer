177a339b0d0fdab4d9b826c7c47981b491236e3b
Leaf <spacecats64@gmail.com>
2021-12-23 22:29:36 -0500

    fix utf8 conversion and make it use proper api for given system

--- src/GenUtil.cpp
+++ src/GenUtil.cpp
@@ -25,6 +25,7 @@ std::string LGenUtility::Utf8ToSjis(const std::string& value)
 
 std::string LGenUtility::SjisToUtf8(const std::string& value)
 {
+#ifdef __linux__
     icu::UnicodeString src(value.c_str(), "shift_jis");
     int length = src.extract(0, src.length(), NULL, "utf8");
 
@@ -32,4 +33,51 @@ std::string LGenUtility::SjisToUtf8(const std::string& value)
     src.extract(0, src.length(), &result[0], "utf8");
 
     return std::string(result.begin(), result.end() - 1);
+#endif
+
+
+#ifdef _WIN32
+    if (value.empty())
+        return "";
+
+    UErrorCode errorCode;
+    UConverter* conv = ucnv_open("shift_jis", &errorCode);
+
+    std::vector<UChar> uCharBuff;
+    const char* a = value.c_str();
+
+    // Convert the input from SHIFT-JIS to UTF-16.
+    // TODO: Is there a better way to do this?
+    UChar target;
+    errorCode = UErrorCode::U_ZERO_ERROR;
+    while (errorCode == UErrorCode::U_ZERO_ERROR)
+    {
+        errorCode = UErrorCode::U_ZERO_ERROR;
+        target = ucnv_getNextUChar(conv, &a, (&value.back()) + 1, &errorCode);
+
+        if (errorCode == UErrorCode::U_ZERO_ERROR)
+            uCharBuff.push_back(target);
+    }
+
+    // Ensure there's a null terminator
+    uCharBuff.push_back(0);
+
+    ucnv_close(conv);
+
+    int32_t outputSize;
+    std::string output;
+
+    // Figure out how much space we need to convert from UTF-16 to UTF-8.
+    errorCode = UErrorCode::U_ZERO_ERROR;
+    u_strToUTF8(nullptr, 0, &outputSize, &uCharBuff.front(), uCharBuff.size(), &errorCode);
+    
+    // Make the string the string big enough to receive the converted data.
+    output.resize(static_cast<size_t>(outputSize));
+
+    // Actually convert the data.
+    errorCode = UErrorCode::U_ZERO_ERROR;
+    u_strToUTF8(output.data(), outputSize, &outputSize, &uCharBuff[0], uCharBuff.size(), &errorCode);
+
+    return output;
+#endif
 }
